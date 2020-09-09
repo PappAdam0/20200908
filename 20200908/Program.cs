5101a453de2180b203aa455a7ff8772b3806203f
Ádám Papp <papp.adaam@gmail.com>
2020-09-09 19:07:50 +0200

    befejezes

--- 20200908/Program.cs
+++ 20200908/Program.cs
@@ -2,66 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace _20200908
 {
     class Program
     {
-        static void Main(string[] args)
+        static string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
+        static void eredmeny_kiiras(int gep, int ember)
+        {
+            Console.WriteLine("Gép {0} --- Játékos {1}", lehetoseg[gep], lehetoseg[ember]);
+            switch (GepNyer(gep, ember))
+            {
+                case 0:
+                    Console.WriteLine("Döntetelen");
+                    break;
+                case 1:
+                    Console.WriteLine("Skynet nyert");
+                    break;
+                case 2:
+                    Console.WriteLine("Játékos nyert");
+                    break;
+            }
+        }
+        static int GepNyer(int gep, int ember)
+        {
+            if (ember == 0 && gep == 1 //Gép nyer
+                ||
+                (ember == 1 && gep == 2)
+                ||
+                (ember == 2 && gep == 0)
+                )
+            {
+                return 1;
+            }
+            else if (gep == ember) //Döntetlen
+            {
+                return 0;
+            }
+            else //Játékos nyer
+            {
+                return 2;
+            }
+        }
+        static int jatekos_valaszt()
+        {
+            Console.WriteLine("Kő {0}, Papír {1}, Olló {2}");
+            Console.Write("Válasz: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        static int gepvalasztas()
         {
             Random vel = new Random();
-            string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
-            int gepValasz = vel.Next(0, 3);
-          
-
-            int jatekosValasz;
-            Console.WriteLine("Kő (0), Papír (1), Olló (2)");
-            Console.Write("Válasz: ");
-            jatekosValasz = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Játékos választása: {0}",lehetoseg[jatekosValasz]);
-            Console.WriteLine("Gép Választása: {0}", lehetoseg[gepValasz]);
-
-            if (lehetoseg[jatekosValasz]=="Olló" && lehetoseg[gepValasz] == "Kő")
-            {
-                Console.WriteLine("Veszítettél");
-            }
-            if (lehetoseg[jatekosValasz] == "Olló" && lehetoseg[gepValasz] == "Papír")
-            {
-                Console.WriteLine("Nyertél");
-            }
-            if (lehetoseg[jatekosValasz] == "Olló" && lehetoseg[gepValasz] == "Olló")
-            {
-                Console.WriteLine("Döntetlen");
-            }
-            if (lehetoseg[jatekosValasz] == "Kő" && lehetoseg[gepValasz] == "Kő")
-            {
-                Console.WriteLine("Döntetlen");
-            }
-            if (lehetoseg[jatekosValasz] == "Kő" && lehetoseg[gepValasz] == "Papír")
-            {
-                Console.WriteLine("Vesztettél");
-            }
-            if (lehetoseg[jatekosValasz] == "Kő" && lehetoseg[gepValasz] == "Olló")
-            {
-                Console.WriteLine("Nyertél");
-            }
-            if (lehetoseg[jatekosValasz] == "Papír" && lehetoseg[gepValasz] == "Kő")
-            {
-                Console.WriteLine("Nyertél");
-            }
-            if (lehetoseg[jatekosValasz] == "Papír" && lehetoseg[gepValasz] == "Papír")
-            {
-                Console.WriteLine("Döntetlen");
-            }
-            if (lehetoseg[jatekosValasz] == "Papír" && lehetoseg[gepValasz] == "Olló")
-            {
-                Console.WriteLine("Vesztettél");
-            }
-
-
+            return vel.Next(0, 3);
+        }
+        static void Main(string[] args)
+        {
+            //gépválasztása        
+            //Console.WriteLine("valasztasok {0} ", lehetoseg[gep_valaszt]);
+            //játékosválasztása
+            int jatekosvalaszt = jatekos_valaszt();
+            int gep_valaszt = gepvalasztas();
+            eredmeny_kiiras(gep_valaszt, jatekosvalaszt);
             Console.ReadKey();
         }
     }
