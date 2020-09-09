b2b82b5d963c997bc95571dc441d72aa18170d6c
Ádám Papp <papp.adaam@gmail.com>
2020-09-09 06:32:27 +0200

    Kiirja az eredményt

--- 20200908/Program.cs
+++ 20200908/Program.cs
@@ -14,14 +14,54 @@ namespace _20200908
             Random vel = new Random();
             string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
             int gepValasz = vel.Next(0, 3);
-            Console.WriteLine("Gép Választása: {0}",lehetoseg[gepValasz]);
+          
 
             int jatekosValasz;
             Console.WriteLine("Kő (0), Papír (1), Olló (2)");
             Console.Write("Válasz: ");
             jatekosValasz = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Játékos választása: {0}",lehetoseg[jatekosValasz]);
-            
+            Console.WriteLine("Gép Választása: {0}", lehetoseg[gepValasz]);
+
+            if (lehetoseg[jatekosValasz]=="Olló" && lehetoseg[gepValasz] == "Kő")
+            {
+                Console.WriteLine("Veszítettél");
+            }
+            if (lehetoseg[jatekosValasz] == "Olló" && lehetoseg[gepValasz] == "Papír")
+            {
+                Console.WriteLine("Nyertél");
+            }
+            if (lehetoseg[jatekosValasz] == "Olló" && lehetoseg[gepValasz] == "Olló")
+            {
+                Console.WriteLine("Döntetlen");
+            }
+            if (lehetoseg[jatekosValasz] == "Kő" && lehetoseg[gepValasz] == "Kő")
+            {
+                Console.WriteLine("Döntetlen");
+            }
+            if (lehetoseg[jatekosValasz] == "Kő" && lehetoseg[gepValasz] == "Papír")
+            {
+                Console.WriteLine("Vesztettél");
+            }
+            if (lehetoseg[jatekosValasz] == "Kő" && lehetoseg[gepValasz] == "Olló")
+            {
+                Console.WriteLine("Nyertél");
+            }
+            if (lehetoseg[jatekosValasz] == "Papír" && lehetoseg[gepValasz] == "Kő")
+            {
+                Console.WriteLine("Nyertél");
+            }
+            if (lehetoseg[jatekosValasz] == "Papír" && lehetoseg[gepValasz] == "Papír")
+            {
+                Console.WriteLine("Döntetlen");
+            }
+            if (lehetoseg[jatekosValasz] == "Papír" && lehetoseg[gepValasz] == "Olló")
+            {
+                Console.WriteLine("Vesztettél");
+            }
+
+
             Console.ReadKey();
         }
     }
